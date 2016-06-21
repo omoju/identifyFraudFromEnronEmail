bee1dffd349ce08bae7b693263a0bcebe63a3346
Omoju <omojumiller@gmail.com>
2016-06-20 17:15:22 -0700

    fixed: compareTwoFeatures
    Now returns the datapoint from the scatterplot

--- outlier_detection.py
+++ outlier_detection.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 def compareTwoFeatures(feature1, feature2, data_dict):
     """
-    Compare two features in a scatter plot.
+    Compare two features in a scatter plot and returns the datapoints
    
     Keyword arguments:
     feature1 -- an (n x k) numpy array where n is 2 and k datapoints 
@@ -25,4 +25,11 @@ def compareTwoFeatures(feature1, feature2, data_dict):
 
     plt.xlabel(feature1)
     plt.ylabel(feature2)
+    plt.grid(True)
+    _= plt.title('Comparing two features for outliers')
     plt.show() 
+    
+    return data
+
+    
+    
